a11c985293fcca16a4abd359bb4842d7935d7d96
stropee <stropee@campus-eni.fr>
2026-01-20 09:31:56 +0100

    TP 1,2,3

--- src/app/components/carbon-footprint/carbon-footprint.ts
+++ src/app/components/carbon-footprint/carbon-footprint.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {CarbonFootprintForm} from '../carbon-footprint-form/carbon-footprint-form';
 import {CarbonFootprintResult} from '../carbon-footprint-result/carbon-footprint-result';
 import {DecimalPipe} from '@angular/common';
@@ -15,29 +15,53 @@ import {DecimalPipe} from '@angular/common';
 })
 export class CarbonFootprint {
 
-  public readonly MAX_CONSUMPTION  : number = 7;
-  public readonly MIN_CONSUMPTION  : number = 4;
+  public readonly MAX_CONSUMPTION: number = 7;
+  public readonly MIN_CONSUMPTION: number = 4;
 
-  public distance : number
-  public consumptionPer100 : number
-  public travels : {distance : number, consumptionPer100 : number}[]
+  public distance: number
+  public consumptionPer100: number
+  public travels: { distance: number, consumptionPer100: number }[]
 
   constructor() {
+
+    this.distance = 0;
+    this.consumptionPer100 = 0
+
     this.travels = [
-        { distance: 50, consumptionPer100: 5 },
-        { distance: 150, consumptionPer100: 6 },
-        { distance: 250, consumptionPer100: 7 },
-        { distance: 350, consumptionPer100: 8 },
-        { distance: 450, consumptionPer100: 9 }
+      {distance: 50, consumptionPer100: 5},
+      {distance: 150, consumptionPer100: 6},
+      {distance: 250, consumptionPer100: 7},
+      {distance: 350, consumptionPer100: 8},
+      {distance: 450, consumptionPer100: 9}
     ];
-    this.distance = 123;
-    this.consumptionPer100 = 657.45;
+    this.calculateDistanceAndConsumptionAverage()
   }
 
   add100km() {
-    if(this.distance){
+    if (this.distance) {
       this.distance += 100;
     }
 
   }
+
+  addTravel() {
+    const distance = Math.round(Math.random() * 1000)
+    const consumption = Math.round(Math.random() * 10)
+    this.travels.push({distance: distance, consumptionPer100: consumption})
+    this.calculateDistanceAndConsumptionAverage()
+  }
+
+  private calculateDistanceAndConsumptionAverage() {
+
+    this.distance = this.travels.reduce(
+      (acc, travel) => {
+        return acc + travel.distance;
+      }, 0)
+
+    this.consumptionPer100 = this.travels.reduce(
+      (acc, travel) => {
+        return acc + travel.consumptionPer100;
+      }, 0) / this.travels.length;
+  }
+
 }
